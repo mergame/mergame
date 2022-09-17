517723b2687ed9336871574935bf843ca765d5b2
shikai <merge2077@gmail.com>
2022-09-17 17:47:47 +0800

    update list

--- assets/README.1e91a8df.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./app.450c02a5.js";const e={},o=s(e,[["__file","/Users/ks/code/wb/merger/src/pages/README.md"]]);export{o as default};
---
+++ assets/README.fc99b289.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./app.604f83cf.js";const e={},o=s(e,[["__file","/Users/ks/code/wb/merger/src/pages/README.md"]]);export{o as default};
