a897218838214f934d39d24eaabadadd9bfa0eeb
shikai <merge2077@gmail.com>
2022-09-17 14:45:59 +0800

    fix bug

--- assets/README.0ad58631.js
+++
@@ -1,1 +0,0 @@
-import{_ as s}from"./app.1ceec6cd.js";const e={},o=s(e,[["__file","/Users/ks/code/wb/merger/src/pages/README.md"]]);export{o as default};
---
+++ assets/README.a69967b0.js
@@ -0,0 +1,1 @@
+import{_ as s}from"./app.9eec64f3.js";const e={},o=s(e,[["__file","/Users/ks/code/wb/merger/src/pages/README.md"]]);export{o as default};
